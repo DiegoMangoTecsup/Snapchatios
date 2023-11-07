709c520fa992b6ad359a997f171de91d01457fe9
DiegoMangoTecsup <diego.mango@tecsup.edu.com>
2023-11-07 17:06:24 -0500

    Initial Commit

---
+++ MangoSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MangoSnapchat
+//
+//  Created by Diego Freddy Mango Rosas on 7/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
