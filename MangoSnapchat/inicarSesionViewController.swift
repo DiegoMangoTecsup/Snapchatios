b392b121ba06db2c64055cbd19f701f8abdc74e8
DiegoMangoTecsup <diego.mango@tecsup.edu.com>
2023-11-07 18:48:47 -0500

    AvanceLab

---
+++ MangoSnapchat/inicarSesionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MangoSnapchat
+//
+//  Created by Diego Freddy Mango Rosas on 7/11/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class iniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
